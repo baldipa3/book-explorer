8e6ae416a430f500779de3fe14182e6b60c2759d
Pablo Gaston Baldini <pablo@Pablos-MacBook-Air.local>
2022-12-07 14:22:19 -0300

    Devise views and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :csv_files
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
