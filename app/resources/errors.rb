4cce1b39edfba2870b686c9c0407454a94494892
Pablo Gaston Baldini <pablo@Pablos-MacBook-Air.local>
2022-12-10 22:09:58 -0300

    Conflicts resolved

--- app/resources/errors.rb
+++ app/resources/errors.rb
@@ -1,4 +1,10 @@
 module Errors
+  class MissingHeadersError < StandardError
+    def message
+      "Headers are missing for this file"
+    end
+  end
+
   class ValidationError < StandardError
     def initialize(id)
       super
@@ -7,12 +13,6 @@ module Errors
 
     def message
       "A book with book_id: #{@id} is already Uploaded"
-    end
-  end
-
-  class MissingHeadersError < StandardError
-    def message
-      "Headers are missing for this file"
     end
   end
 end
